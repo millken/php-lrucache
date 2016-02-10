aa29b616cf873b76d18ee0545f8784a0512a462b
justinrobinson <justinrobinson@bizjournals.com>
2016-02-10 08:51:31 -0500

    [master] remove broken phpunit from composer

--- test/LRUTest.php
+++ test/LRUTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once('PHPUnit/Autoload.php');
+//require_once('PHPUnit/Autoload.php');
 require_once(dirname(__FILE__).'/../src/LRUCache/LRUCache.php');
 
 class LRUCacheTest extends PHPUnit_Framework_TestCase {
